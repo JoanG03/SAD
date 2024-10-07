ba2b9a2d32602c9937067d048cd9566bd4b8e51b
JoanG03 <132643368+JoanG03@users.noreply.github.com>
2024-10-07 11:11:24 +0200

    Update EditableBufferedReader.java

--- EditableBufferedReader.java
+++ EditableBufferedReader.java
@@ -3,65 +3,75 @@ import java.io.Reader;
 import java.io.BufferedReader;
 
 public class EditableBufferedReader extends BufferedReader{
-
-public EditableBufferedReader(Reader text){
-    super(text);
-    this.vista= new View();
-}
-
-public void setRaw() throws IOException{ 
-    try{
-        String[] comm = {"/bin/sh", "-c", "stty raw -echo </dev/tty"}; 
-
-    }catch(InterruptedException ex){
-        ex.printStackTrace();
-    }
-}
-
-public void unsetRaw() throws IOException{  
-    try{
-        String[] comm = {"/bin/sh", "-c", "stty cooked echo </dev/tty"};
-        Runtime.getRuntime().exec(comm).waitFor();
  
-    }catch(InterruptedException ex){
-        ex.printStackTrace();
-    }
-}
 
-public int read() throws IOException{
-    int caracter = 0;
-    setRaw();
-    caracter = super.read();
-    if(caracter == Keys.ESC){
-        caracter = super.read(); 
-        if(caracter == Keys.ESTILO){
-            caracter = Keys.xESTILO;
-            return caracter;
+    public EditableBufferedReader(Reader text){
+        super(text);
+    }
+
+    public void setRaw() throws IOException{ 
+        try{
+            String[] comm = {"/bin/sh", "-c", "stty raw -echo </dev/tty"}; 
+
+        }catch(InterruptedException ex){
+            Thread.currentThread().interrupt();
+            ex.printStackTrace();
         }
-        caracter= super.read();
-        if(caracter>64){//Si després llegim una lletra
-            switch (caracter) {
-                case Keys.INSERT: caracter = Keys.xINSERT;
-                    break;
-                case Keys.RIGHT: caracter = Keys.xRIGHT;
-                    break;
-                case Keys.LEFT: caracter = Keys.xLEFT;
-                    break;
-                case Keys.INICIO: caracter= Keys.xINICIO;
-                    break;
-                case Keys.FIN: caracter= Keys.xFIN;
-                    break;
-            } 
-        }else{ //Si llegim un número
-            switch (caracter){
-                case Keys.INSERT: caracter=Keys.xINSERT;
-                    break;
-                case Keys.SUPR: caracter= Keys.xSUPR;
-                    break;
+    }
+
+    public void unsetRaw() throws IOException{  
+        try{
+            String[] comm = {"/bin/sh", "-c", "stty cooked echo </dev/tty"};
+            Runtime.getRuntime().exec(comm).waitFor();
+    
+        }catch(InterruptedException ex){
+            Thread.currentThread().interrupt();
+            ex.printStackTrace();
+        }
+    }
+
+    public int read() throws IOException{
+        int caracter = 0;
+        setRaw();
+        caracter = super.read();
+
+        if(caracter == Keys.ESC){
+            caracter = super.read(); 
+            if(caracter == Keys.ESTILO){
+                caracter = 
+                return caracter;
             }
-            super.read(); //Obviem el "~"
-        }
-    }
 
-    return caracter;
-}
+            caracter= super.read();
+        
+                switch (caracter) {
+                    case Keys.INSERT:
+                        caracter = 
+                        break;
+                    case Keys.RIGHT:
+                        caracter =
+                        break;
+                    case Keys.LEFT:
+                        caracter =
+                        break;
+                    case Keys.INICIO:
+                        caracter = 
+                        break;
+                    case Keys.FIN:
+                        caracter = 
+                        break;
+                    case Keys.SUPR:
+                        caracter = 
+                        break;
+                    default:
+                        // Omite el "~" si es una secuencia de escape numérica
+                        if (caracter <= 64) {
+                            super.read();
+                        }
+                        break;
+                }
+            }
+
+            unsetRaw();
+            return caracter;
+    }
