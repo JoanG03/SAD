5bdaaf7a594552904ee26c290a441964148b7ad3
JoanG03 <132643368+JoanG03@users.noreply.github.com>
2024-10-15 17:20:48 +0200

    Update Keys.java

--- Keys.java
+++ Keys.java
@@ -1,7 +1,7 @@
 interface Keys {
 
 // Codificaciones de las secuencias de escape 
-
+    int DELETE = 8;
     int RETURN = 13;
     int BACKSPACE = 127;
 
@@ -16,11 +16,8 @@ interface Keys {
     int UP = 65;            // ^[[A  (Flecha arriba)
     int DOWN = 66;          // ^[[B  (Flecha abajo)
     int FIN = 70;           // ^[[F  (Fin)
-    int INICIO = 72;        // ^[[H  (Inicio)
+    int HOME = 72;        // ^[[H  (Home)
     int INSERT = 50;        // ^[[2~ (Insertar)
-    int AVPAG = 54;         // ^[[6~ (Avanzar página)
-    int REPAG = 53;         // ^[[5~ (Retroceder página)
     int SUPR = 51;          // ^[[3~ (Suprimir)
-    int ESTILO = 101;       // ^[e   (Estilo)
 
 }
