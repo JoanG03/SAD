13fed9c6428691d200754352db19a8b83af1282d
JoanG03 <132643368+JoanG03@users.noreply.github.com>
2024-11-19 18:45:44 +0100

    Create MyServerSocket.java

---
+++ P1/MyServerSocket.java
@@ -0,0 +1,24 @@
+package P1;
+import java.net.ServerSocket;
+import java.io.*;
+
+
+public class MyServerSocket {
+    private ServerSocket ss;
+
+    public MyServerSocket(int port) throws IOException {
+        this.ss = new ServerSocket(port);
+    }
+
+    public MySocket accept() throws IOException{
+       return new MySocket(ss.accept());       
+    }
+
+    public void close() {
+        try {
+            ss.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
