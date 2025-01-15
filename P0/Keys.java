8b1e38d21f8392e2fc0be64e3a68fccc92a653df
JoanG03 <132643368+JoanG03@users.noreply.github.com>
2025-01-15 17:16:12 +0100

    Update Keys.java

--- P0/Keys.java
+++ P0/Keys.java
@@ -1,1 +1,23 @@
+package P0;
+interface Keys {
 
+    // Codificaciones de las secuencias de escape 
+    int RETURN = 13;
+    int BACKSPACE = 127;
+
+    int ESC = 27;           // ESC => ^[
+    int CSI = 91;           // CSI => [
+
+
+    // Funciones con ESC + CSI
+
+    int RIGHT = 67;         // ^[[C  (Flecha derecha)
+    int LEFT = 68;          // ^[[D  (Flecha izquierda)
+    int UP = 65;            // ^[[A  (Flecha arriba)
+    int DOWN = 66;          // ^[[B  (Flecha abajo)
+    int FIN = 70;           // ^[[F  (Fin)
+    int HOME = 72;        // ^[[H  (Home)
+    int INSERT = 50;        // ^[[2~ (Insertar)
+    int SUPR = 51;          // ^[[3~ (Suprimir)
+    
+}
